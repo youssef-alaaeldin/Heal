21d525000ef0ff817dba7a5baa1f3615d5ca2888
YoussefAlaaeldin <yousufalaa10@yahoo.com>
2024-05-19 16:43:18 +0300

    finishing Sleeping Analysis View

---
+++ Heal/View/SleepingAnalysisView.swift
@@ -0,0 +1,18 @@
+//
+//  SleepingAnalysisView.swift
+//  Heal
+//
+//  Created by Yousuf Abdelfattah on 19/05/2024.
+//
+
+import SwiftUI
+
+struct SleepingAnalysisView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SleepingAnalysisView()
+}
